fb78892c967b3fbc8abbec1fb1bd4d0398b862e1
Bokao Junior Matende <Juniorbokao2006@gmail.com>
2026-08-30 15:48:12 +0100

    Add Question 1: personal profile

---
+++ Question1.java
@@ -0,0 +1,14 @@
+public class Question1 {
+    public static void main(String[] args) {
+        // Values are set directly in code - no user input needed
+        String name = "Bokao Matende";
+        int age = 20;
+        double gpa = 2.5;
+        boolean fullTime = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Full-time: " + fullTime);
+    }
+}
