0d9f98753fc09151e15d72ab18964ab296acb956
Bokao Junior Matende <Juniorbokao2006@gmail.com>
2026-08-30 16:11:31 +0100

    Add Question 5: Simple Calculator

---
+++ Question5.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Question5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Read as double so quotient keeps decimal places instead of truncating
+        System.out.print("Enter the first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = sc.nextDouble();
+
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        double quotient = num1 / num2;
+        double remainder = num1 % num2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+
+        sc.close();
+    }
+}
